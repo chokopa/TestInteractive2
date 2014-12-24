9089cff0eaf2ac2be9e59cbd8caeefe168632604
shinsuke tomita <shinsuke.tomita@bizreach.co.jp>
2014-12-24 17:54:18 +0900

    Initial Commit

---
+++ TestInteractive2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestInteractive2
+//
+//  Created by shinsuke.tomita@bizreach.co.jp on 2014/12/24.
+//  Copyright (c) 2014年 shinsuke.tomita@bizreach.co.jp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
